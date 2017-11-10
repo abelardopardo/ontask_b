6f3c3977aa8ca04133f4eacb9f663cd8c244e065
Abelardo Pardo <abelardo.pardo@gmail.com>
2017-11-10 19:19:37 +1100

    Tweak class

--- src/ontask/settings/base.py
+++ src/ontask/settings/base.py
@@ -118,7 +118,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django.middleware.security.SecurityMiddleWare',
+    'django.middleware.security.SecurityMiddleware',
 )
 
 AUTHENTICATION_BACKENDS = [
