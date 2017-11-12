c9d227a16d1fc9486ff73cc7dda89e0ec52eb801
Abelardo Pardo <abelardo.pardo@gmail.com>
2017-11-13 07:47:12 +1100

    Added missing class in config

--- src/ontask/settings/base.py
+++ src/ontask/settings/base.py
@@ -147,6 +147,7 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
     ),
     'DEFAULT_PAGINATION_CLASS':
         'rest_framework.pagination.LimitOffsetPagination',
