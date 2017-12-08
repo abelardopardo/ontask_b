be09548ea629357946e6c575b2cf288e8553a4ba
Abelardo Pardo <abelardo.pardo@gmail.com>
2017-12-08 17:45:09 +1100

    Fixed JS caching by adding a directive that propagates the version. There is a more elegant solution (using folders), but could be done later

--- src/ontask/templatetags/settings.py
+++ src/ontask/templatetags/settings.py
@@ -12,3 +12,13 @@ register = template.Library()
 @register.simple_tag
 def ontask_version():
     return ontask.__version__
+
+
+@register.simple_tag
+def otv():
+    """
+    Function to return a URL query parameter with the ontask version to force
+    refresh of javascript URLs
+    :return: The query param to add to the end of a URL
+    """
+    return '?v=' + ontask.__version__
