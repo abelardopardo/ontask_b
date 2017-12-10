7f586559c3edfe6526ddb38af82bdc994d882825
Abelardo Pardo <abelardo.pardo@gmail.com>
2017-12-11 06:13:31 +1100

    Removed processing of "remember_me" as it prolongs sessions for one month.

--- src/accounts/views.py
+++ src/accounts/views.py
@@ -19,11 +19,11 @@ class LoginView(bracesviews.AnonymousRequiredMixin,
 
     def form_valid(self, form):
         redirect = super(LoginView, self).form_valid(form)
-        remember_me = form.cleaned_data.get('remember_me')
-        if remember_me is True:
-            ONE_MONTH = 30*24*60*60
-            expiry = getattr(settings, "KEEP_LOGGED_DURATION", ONE_MONTH)
-            self.request.session.set_expiry(expiry)
+        # remember_me = form.cleaned_data.get('remember_me')
+        # if remember_me is True:
+        #     ONE_MONTH = 30*24*60*60
+        #     expiry = getattr(settings, "KEEP_LOGGED_DURATION", ONE_MONTH)
+        #     self.request.session.set_expiry(expiry)
         return redirect
 
 
