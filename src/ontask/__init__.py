24a9d0b33416915034727e4fbdc73ea56c74a4ac
Abelardo Pardo <abelardo.pardo@gmail.com>
2017-12-22 09:57:15 +1100

    Version bump

--- src/ontask/__init__.py
+++ src/ontask/__init__.py
@@ -1,9 +1,11 @@
 # -*- coding: utf-8 -*-
+"""
+Basic functions and definitions used all over the platform.
+"""
 from __future__ import unicode_literals, print_function
 
-import re
+__version__ = 'B.2.2.0'
 
-__version__ = 'B.2.1.1'
 
 def is_legal_name(val):
     """
@@ -11,7 +13,7 @@ def is_legal_name(val):
     condition name.
 
     These are the characters that have been found to be problematic with
-    these names and the responsibles for these anomalies:
+    these names and the responsible for these anomalies:
 
     - " Provokes a db error when handling the templates due to the encoding
       produced by the text editor.
@@ -37,16 +39,24 @@ def is_legal_name(val):
 
     return None
 
+
 def fix_pctg_in_name(val):
+    """
+    Function that escapes a value for SQL processing (replacing % by double %%)
+    :param val: Value to escape
+    :return: Escaped value
+    """
     return val.replace('%', '%%')
 
 
 class OntaskException(Exception):
+    """
+    Generic class in OnTask for our own exception
+    """
+
     def __init__(self, msg, value):
         self.msg = msg
         self.value = value
 
     def __str__(self):
         return repr(self.value)
-
-
