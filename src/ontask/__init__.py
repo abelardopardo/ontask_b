fae933d4976937a4065940a0304b9760260fdc7a
Abelardo Pardo <abelardo.pardo@gmail.com>
2017-12-28 11:48:10 +1100

    Version bump

--- src/ontask/__init__.py
+++ src/ontask/__init__.py
@@ -4,7 +4,7 @@ Basic functions and definitions used all over the platform.
 """
 from __future__ import unicode_literals, print_function
 
-__version__ = 'B.2.2.0'
+__version__ = 'B.2.2.1'
 
 
 def is_legal_name(val):
