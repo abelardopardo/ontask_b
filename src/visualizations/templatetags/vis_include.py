6a8cdd63ea411ee41e772d96ac04c315d0f50880
Abelardo Pardo <abelardo.pardo@gmail.com>
2018-02-22 09:43:15 +1100

    Major changes to implement a custom tag to include a visualization of a histogram with population data and a mark for an individual data. Not fully implemented yet. It needs additional functionality so that it is visible for each student, and to accommodate types of visualisations for each column.

--- src/visualizations/templatetags/vis_include.py
+++ src/visualizations/templatetags/vis_include.py
@@ -2,31 +2,69 @@
 from __future__ import unicode_literals
 
 from django import template
-from django.utils.html import format_html
+from django.core.exceptions import ObjectDoesNotExist
+from django.utils.safestring import mark_safe
 
+from action.evaluate import action_context_var, viz_number_context_var, tr_item
+from action.models import Condition
+from dataops import pandas_db
+from visualizations.plotly import PlotlyColumnHistogram
 from workflow.models import Column
-from workflow.ops import get_workflow
 
 register = template.Library()
 
-def vis_html_content(context, column_name, vtype=None):
-    # Get the workflow
-    workflow = get_workflow(context['request'])
-    if not workflow:
-        raise Exception('Workflow object not found in request')
+
+def vis_html_content(context, column_name):
+    # Get the action
+    action = context.get(action_context_var, None)
+    if not action:
+        raise Exception('Action object not found when processing tag')
+    workflow = action.workflow
 
     # Check if the column is correct
     if not Column.objects.filter(workflow=workflow, name=column_name).exists():
         raise Exception('Column {0} does not exist'.format(column_name))
 
-    if vtype not in [None, 'histogram', 'boxplot']:
-        raise Exception('Visualization type must be histogram or boxplot')
+    # Get the visualization number to generate unique IDs
+    viz_number = context[viz_number_context_var]
 
-    # TO BE DESIGNED HOW THE MACRO IS THEN EVALUATED WITH THE RIGHT PERSONAL
-    # DATA. NO IDEA HOW TO DO IT SO FAR.
-    return format_html('<p>{0} {1}</p>'.format(column_name, vtype))
+    # Create the context for the visualization
+    viz_ctx = {
+        'style': 'width:400px; height:225px;',
+        'id': 'viz_tag_{0}'.format(viz_number)
+    }
+
+    # If there is a column name in the context, insert it as individual value
+    # If the template is simply being saved and rendered to detect syntax
+    # errors, we may not have the data of an individual, so we have to relax
+    # this restriction.
+    ivalue = context.get(tr_item(column_name), None)
+    if ivalue:
+        viz_ctx['individual_value'] = ivalue
+
+    # Get the condition filter
+    try:
+        cond_filter = Condition.objects.get(action__id=action.id,
+                                            is_filter=True)
+    except ObjectDoesNotExist:
+        cond_filter = None
+
+    # Get the data from the data frame
+    df = pandas_db.get_subframe(workflow.id,
+                                cond_filter,
+                                [column_name])
+    # Get the visualisation
+    viz = PlotlyColumnHistogram(data=df,
+                                context=viz_ctx)
+
+    # Update viz number
+    context[viz_number_context_var] = viz_number + 1
+
+    # Return the rendering of the viz marked as safe
+    return mark_safe(viz.render())
+
 
 # Register the tag in the library.
 register.simple_tag(func=vis_html_content,
                     takes_context=True,
-                    name = 'visualization')
+                    name='visualization')
