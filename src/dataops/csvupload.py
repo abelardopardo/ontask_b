621004d65e8fdf840fe3820b9476fc355fc3b857
Abelardo Pardo <abelardo.pardo@gmail.com>
2018-04-08 09:45:17 +0930

    Reverted forced casting from integer to double. Just report the type as number and let pandas handle internally all these cases.

--- src/dataops/csvupload.py
+++ src/dataops/csvupload.py
@@ -84,11 +84,11 @@ def csvupload1(request):
         # Strip white space from all string columns and try to convert to
         # datetime just in case
         for x in list(data_frame.columns):
-            if data_frame[x].dtype.name.startswith('int'):
-                # Columns that only have integers are translated to double to
-                # prevent unexpected changes due to the presence of NA.
-                data_frame[x] = data_frame[x].astype('double')
-                continue
+            # if data_frame[x].dtype.name.startswith('int'):
+            #     # Columns that only have integers are translated to double to
+            #     # prevent unexpected changes due to the presence of NA.
+            #     data_frame[x] = data_frame[x].astype('double')
+            #     continue
 
             if data_frame[x].dtype.name == 'object':
                 # Column is a string! Remove the leading and trailing white
