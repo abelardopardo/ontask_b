eccf0e4770dd5a08f5c4a56b2b71073f735d6d9b
Abelardo Pardo <abelardo.pardo@gmail.com>
2018-05-20 20:12:32 +0930

    Changed fields related to n_rows_selected (issue #26)

--- src/scheduler/admin.py
+++ src/scheduler/admin.py
@@ -2,13 +2,13 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+
 from models import ScheduledEmailAction
 
 
 class ScheduledEmailActionAdmin(admin.ModelAdmin):
     list_display = ('user', 'type', 'created', 'execute',
                     'status', 'action', 'subject', 'email_column',
-                    'send_confirmation', 'track_read', 'add_column',
-                    'message')
+                    'send_confirmation', 'track_read', 'message')
 
 admin.site.register(ScheduledEmailAction, ScheduledEmailActionAdmin)
