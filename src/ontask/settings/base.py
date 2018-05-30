e468390c16940e8b316cdf7313ca04e6da14f93f
Abelardo Pardo <abelardo.pardo@gmail.com>
2018-05-30 18:35:23 +0930

    Using now Argon2 as password has.

--- src/ontask/settings/base.py
+++ src/ontask/settings/base.py
@@ -135,6 +135,14 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.locale.LocaleMiddleware'
 )
 
+PASSWORD_HASHERS = [
+    'django.contrib.auth.hashers.Argon2PasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
+    'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
+    'django.contrib.auth.hashers.BCryptPasswordHasher',
+]
+
 #
 # LDAP AUTHENTICATION
 #
