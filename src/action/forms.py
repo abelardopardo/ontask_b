ea0d1f70c13c9c8ae86e630b3fe808b32e68fb26
Abelardo Pardo <abelardo.pardo@gmail.com>
2018-07-20 15:34:45 +0930

    Fixed use of variables in templates

--- src/action/forms.py
+++ src/action/forms.py
@@ -6,7 +6,7 @@ import json
 from datetimewidget.widgets import DateTimeWidget
 from django import forms
 from django_summernote.widgets import SummernoteInplaceWidget
-from django.utils.translation import ugettext_lazy as _l
+from django.utils.translation import ugettext_lazy as _
 
 from ontask import ontask_prefs, is_legal_name
 from ontask.forms import column_to_field, dateTimeOptions, RestrictedFileField
@@ -140,11 +140,11 @@ class EnableURLForm(forms.ModelForm):
         if a_from and a_to and a_from >= a_to:
             self.add_error(
                 'active_from',
-                _l('Incorrect date/time window')
+                _('Incorrect date/time window')
             )
             self.add_error(
                 'active_to',
-                _l('Incorrect date/time window')
+                _('Incorrect date/time window')
             )
 
         return data
@@ -167,22 +167,22 @@ class EmailActionBasicForm(forms.Form):
     subject = forms.CharField(max_length=1024,
                               strip=True,
                               required=True,
-                              label=_l('Email subject'))
+                              label=_('Email subject'))
 
     email_column = forms.ChoiceField(
-        label=_l('Column to use for target email address'),
+        label=_('Column to use for target email address'),
         required=True
     )
 
     send_confirmation = forms.BooleanField(
         initial=False,
         required=False,
-        label=_l('Send you a summary message?'))
+        label=_('Send you a summary message?'))
 
     track_read = forms.BooleanField(
         initial=False,
         required=False,
-        label=_l('Track email reading in an extra column?')
+        label=_('Track email reading in an extra column?')
     )
 
     def __init__(self, *args, **kargs):
@@ -211,8 +211,8 @@ class EmailActionForm(EmailActionBasicForm):
     export_wf = forms.BooleanField(
         initial=False,
         required=False,
-        label=_l('Download a snapshot of the current state of the workflow?'),
-        help_text=_l('A zip file useful to review the emails sent.')
+        label=_('Download a snapshot of the current state of the workflow?'),
+        help_text=_('A zip file useful to review the emails sent.')
     )
 
 
@@ -228,5 +228,5 @@ class ActionImportForm(forms.Form):
         max_upload_size=str(ontask_prefs.MAX_UPLOAD_SIZE),
         content_types=json.loads(str(ontask_prefs.CONTENT_TYPES)),
         allow_empty_file=False,
-        label=_l('File'),
-        help_text=_l('File containing a previously exported action'))
+        label=_('File'),
+        help_text=_('File containing a previously exported action'))
