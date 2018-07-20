22bda421aead8a5defa35caa77ef8a28fc06b9ec
Abelardo Pardo <abelardo.pardo@gmail.com>
2018-07-20 17:02:48 +0930

    Modifications after processing table files

--- src/profiles/models.py
+++ src/profiles/models.py
@@ -3,7 +3,7 @@ from django.utils.encoding import python_2_unicode_compatible
 import uuid
 from django.db import models
 from django.conf import settings
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import ugettext as _
 
 
 class BaseProfile(models.Model):
