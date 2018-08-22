9a8301715577230734165f9eccafaf6f05049f83
Abelardo Pardo <abelardo.pardo@gmail.com>
2018-08-22 09:49:34 +0930

    Added extra import to the file to guarantee that signals are imported.

--- src/profiles/apps.py
+++ src/profiles/apps.py
@@ -4,9 +4,11 @@ from __future__ import unicode_literals, print_function
 from django.apps import AppConfig
 from django.utils.translation import ugettext_lazy as _
 
+
 class ProfileConfig(AppConfig):
     name = "profiles"
     verbose_name = _('User Profiles')
 
     def ready(self):
+        # Needed so that the signal registration is done
         pass
