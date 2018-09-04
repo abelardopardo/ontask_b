6b5d9362cc916c4b946760fa3b38ffd086d1110b
Abelardo Pardo <abelardo.pardo@gmail.com>
2018-09-05 07:19:49 +0900

    Additional changes in fixtures to use the right users

--- src/action/fixtures/simple_email_action.sql
+++ src/action/fixtures/simple_email_action.sql
@@ -2,19 +2,18 @@
 -- PostgreSQL database dump
 --
 
--- Dumped from database version 9.6.3
--- Dumped by pg_dump version 9.6.3
+-- Dumped from database version 10.4
+-- Dumped by pg_dump version 10.4
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
 SET idle_in_transaction_session_timeout = 0;
 SET client_encoding = 'UTF8';
 SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
 SET check_function_bodies = false;
 SET client_min_messages = warning;
 SET row_security = off;
-
-SET search_path = public, pg_catalog;
 
 SET default_tablespace = '';
 
@@ -24,7 +23,7 @@ SET default_with_oids = false;
 -- Name: __ONTASK_WORKFLOW_TABLE_5; Type: TABLE; Schema: public; Owner: -
 --
 
-CREATE TABLE "__ONTASK_WORKFLOW_TABLE_5" (
+CREATE TABLE public."__ONTASK_WORKFLOW_TABLE_5" (
     age double precision,
     email text,
     sid bigint,
@@ -41,7 +40,7 @@ CREATE TABLE "__ONTASK_WORKFLOW_TABLE_5" (
 -- Data for Name: __ONTASK_WORKFLOW_TABLE_5; Type: TABLE DATA; Schema: public; Owner: -
 --
 
-COPY "__ONTASK_WORKFLOW_TABLE_5" (age, email, sid, "EmailRead_1", another, name, one, registered, "when") FROM stdin;
+COPY public."__ONTASK_WORKFLOW_TABLE_5" (age, email, sid, "EmailRead_1", another, name, one, registered, "when") FROM stdin;
 12	student1@bogus.com	1	0	bbb	Carmelo Coton	aaa	t	2017-10-11 00:33:44
 12.0999999999999996	student2@bogus.com	2	0	aaa	Carmelo Coton	bbb	f	2017-10-11 00:32:44
 13.1999999999999993	student3@bogus.com	3	0	bbb	Carmelo Coton2	aaa	t	2017-10-11 00:32:44
