86ce27f13763aabba00fce4037bc6aa295d8803f
Abelardo Pardo <abelardo.pardo@gmail.com>
2018-09-12 13:46:46 +0930

    Successful login takes user straight to home page.

--- src/ontask/settings/base.py
+++ src/ontask/settings/base.py
@@ -291,7 +291,7 @@ MESSAGE_TAGS = {
 
 # Authentication Settings
 AUTH_USER_MODEL = 'authtools.User'
-LOGIN_REDIRECT_URL = reverse_lazy("profiles:show_self")
+LOGIN_REDIRECT_URL = reverse_lazy("home")
 LOGIN_URL = reverse_lazy("accounts:login")
 
 THUMBNAIL_EXTENSION = 'png'     # Or any extn for your thumbnails
