dfbb6aa8e5011f4e820ea39ca6138756ef323fca
Abelardo Pardo <abelardo.pardo@gmail.com>
2018-09-16 14:22:57 +0930

    Bump version number

--- src/ontask/__init__.py
+++ src/ontask/__init__.py
@@ -12,7 +12,7 @@ __all__ = ['celery_app', 'OntaskException']
 
 from django.utils.translation import ugettext_lazy as _
 
-__version__ = 'B.2.8.3'
+__version__ = 'B.3.0.0'
 
 
 def is_legal_name(val):
