dbffff5fb6bda78064db2f2f1b9120f55733242e
Abelardo Pardo <abelardo.pardo@gmail.com>
2018-09-21 09:43:08 +0930

    Removed http: prefix from URLs (Issue #85)

--- src/visualizations/plotly.py
+++ src/visualizations/plotly.py
@@ -15,7 +15,7 @@ class PlotlyHandler(VisHandler):
     Handler to produce Plotly visualizations.
     """
 
-    head_scripts = ["http://cdn.plot.ly/plotly-latest.min.js"]
+    head_scripts = ["//cdn.plot.ly/plotly-latest.min.js"]
 
     html_skel = """<div id="{id}" style="{style}"></div>
         <script>Plotly.newPlot('{id}', {data}, 
