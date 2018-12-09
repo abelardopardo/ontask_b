f5ede3bed6a1e518a5989565314004d323de1521
Abelardo Pardo <abelardo.pardo@gmail.com>
2018-12-09 22:19:45 +1030

    Implement OAuth2 token list and deletion from user profile

--- src/profiles/urls.py
+++ src/profiles/urls.py
@@ -11,5 +11,6 @@ urlpatterns = [
     path('me', views.ShowProfile.as_view(), name='show_self'),
     path('me/edit', views.EditProfile.as_view(), name='edit_self'),
     path('reset_token', views.reset_token, name='reset_token'),
+    path('<int:pk>/reset_token/', views.delete_token, name='delete_token'),
     path('<slug:slug>', views.ShowProfile.as_view(), name='show'),
 ]
