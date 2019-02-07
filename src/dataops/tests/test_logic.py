2a6c31059bbd042a6b82df6ca01e37c6732c55fd
Abelardo Pardo <abelardo.pardo@gmail.com>
2019-02-07 11:46:42 +1030

    Adjusted test case to new comparison with Boolean

--- src/dataops/tests/test_logic.py
+++ src/dataops/tests/test_logic.py
@@ -247,10 +247,10 @@ class FormulaEvaluation(test.OnTaskTestCase):
         self.do_operand('number', '{0}equal', 'double', '0.3', 0.3, None)
         self.do_operand('text', '{0}equal', 'string', 'aaa', 'aaa', 'abb')
         self.do_operand('text', '{0}equal', 'string', 'aaa', 'aaa', 'None')
-        self.do_operand('text', '{0}equal', 'boolean', '1', True, False)
-        self.do_operand('text', '{0}equal', 'boolean', '1', None, None)
-        self.do_operand('text', '{0}equal', 'boolean', '0', False, True)
-        self.do_operand('text', '{0}equal', 'boolean', '0', None, None)
+        self.do_operand('text', '{0}equal', 'boolean', True, True, False)
+        self.do_operand('text', '{0}equal', 'boolean', True, None, None)
+        self.do_operand('text', '{0}equal', 'boolean', False, False, True)
+        self.do_operand('text', '{0}equal', 'boolean', False, None, None)
         self.do_operand('text',
                         '{0}equal',
                         'datetime',
@@ -474,7 +474,7 @@ class FormulaEvaluation(test.OnTaskTestCase):
         #
         self.do_sql_operand('number', '{0}equal', 'integer', '1')
         self.do_sql_operand('number', '{0}equal', 'double', '2.0')
-        self.do_sql_operand('number', '{0}equal', 'boolean', '1')
+        self.do_sql_operand('number', '{0}equal', 'boolean', True)
         self.do_sql_operand('text', '{0}equal', 'string', 'xxx')
         self.do_sql_operand('text',
                             '{0}equal',
