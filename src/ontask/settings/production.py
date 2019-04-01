6b176e2e5615af657ba2b3dd34237014f0260bc7
Abelardo Pardo <abelardo.pardo@gmail.com>
2019-04-01 21:50:32 +1030

    Refine configuration for security variables if using SSL

--- src/ontask/settings/production.py
+++ src/ontask/settings/production.py
@@ -5,9 +5,6 @@ import logging.config
 
 from .base import *  # NOQA
 
-# For security and performance reasons, DEBUG is turned off
-DEBUG = False
-
 # Additional middleware introduced by debug toolbar
 if USE_SSL:
     MIDDLEWARE += ['django.middleware.security.SecurityMiddleware']
@@ -18,16 +15,20 @@ if USE_SSL:
 #
 # Security features
 #
-SESSION_COOKIE_SECURE = True
-CSRF_COOKIE_SECURE = True
-SECURE_CONTENT_TYPE_NOSNIFF = True
-SECURE_BROWSER_XSS_FILTER = True
 if USE_SSL:
+    SECURE_CONTENT_TYPE_NOSNIFF = True
+    SECURE_BROWSER_XSS_FILTER = True
+    SESSION_COOKIE_SECURE = True
+    CSRF_COOKIE_SECURE = True
     SECURE_HSTS_SECONDS = 31536000
     SECURE_SSL_REDIRECT = True
     SECURE_HSTS_INCLUDE_SUBDOMAINS = True
     SECURE_HSTS_PRELOAD = True
 else:
+    SECURE_CONTENT_TYPE_NOSNIFF = False
+    SECURE_BROWSER_XSS_FILTER = False
+    SESSION_COOKIE_SECURE = False
+    CSRF_COOKIE_SECURE = False
     SECURE_SSL_REDIRECT = False
 
 #
