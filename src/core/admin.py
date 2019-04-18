1ee6cf8c94937e18e7e3d9235c418b9a2f80722f
Abelardo Pardo <abelardo.pardo@gmail.com>
2019-04-18 16:08:35 +0930

    Extende user model

--- src/core/admin.py
+++ src/core/admin.py
@@ -3,4 +3,9 @@
 
 from django.contrib import admin
 
-# Register your models here.
+from .models import OnTaskUser
+
+class OnTaskUserAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(OnTaskUser, OnTaskUserAdmin)
--- src/core/models.py
+++ src/core/models.py
@@ -1,4 +1,19 @@
 # -*- coding: utf-8 -*-
-
-
+from django.contrib.auth import get_user_model
 from django.db import models
+
+class OnTaskUser(models.Model):
+    """
+    This model is to extend the existing authtools.User with additional fields
+     relevant to OnTask
+    """
+
+    # OneToOne relationship with the authentication model
+    user = models.OneToOneField(
+        get_user_model(),
+        on_delete=models.CASCADE,
+        related_name='ontask_info',
+        primary_key=True,
+    )
+
+
