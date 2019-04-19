1e0c98d36cd9690e99b0020c591c1d361c273224
Abelardo Pardo <abelardo.pardo@gmail.com>
2019-04-19 19:27:53 +0930

    Add string representation of the model

--- src/core/models.py
+++ src/core/models.py
@@ -2,6 +2,7 @@
 from django.contrib.auth import get_user_model
 from django.db import models
 
+
 class OnTaskUser(models.Model):
     """
     This model is to extend the existing authtools.User with additional fields
@@ -16,4 +17,12 @@ class OnTaskUser(models.Model):
         primary_key=True,
     )
 
+    def __str__(self):
+        return self.user.email
 
+    def __unicode__(self):
+        return self.user.email
+
+    class Meta(object):
+        verbose_name = 'ontaskuser'
+        verbose_name_plural = 'ontaskusers'
