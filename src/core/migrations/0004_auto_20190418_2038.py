b34cbdaadaf757c36f7931bb543e8ba4e78c2adf
Abelardo Pardo <abelardo.pardo@gmail.com>
2019-04-19 19:27:34 +0930

    Fix incorrect reference to models in migrations

--- src/core/migrations/0004_auto_20190418_2038.py
+++ src/core/migrations/0004_auto_20190418_2038.py
@@ -2,9 +2,10 @@
 
 from django.db import migrations
 from django.contrib.auth import get_user_model
-from core.models import OnTaskUser
 
 def create_ontask_user_info(apps, schema_editor):
+
+    OnTaskUser = apps.get_model('core', 'OnTaskUser')
 
     # Get all the existing ontask users first
     ousers_id = [x.user.id for x in OnTaskUser.objects.all()]
