0eeeedca0106423d8b764651729bdc3c7c1ed4eb
Abelardo Pardo <abelardo.pardo@gmail.com>
2019-05-13 16:39:20 +0930

    Added type hints

--- src/action/payloads.py
+++ src/action/payloads.py
@@ -2,13 +2,15 @@
 
 """Classes capturing the payloads used when running actions."""
 import collections
+from typing import Dict
 
 from django.conf import settings as ontask_settings
+from django.contrib.sessions.models import Session
 
 action_session_dictionary = 'action_run_payload'
 
 
-def get_action_payload(session):
+def get_action_payload(session: Session) -> Dict:
     """Get the payload from the current session.
 
     :param session: Session object
