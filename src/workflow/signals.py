5dc25b0eb9a41d4d7dccaffdf674f2cab362800f
Abelardo Pardo <abelardo.pardo@gmail.com>
2019-05-28 14:22:11 +0930

    Fixing mistake accessing instance after being deleted.

--- src/workflow/signals.py
+++ src/workflow/signals.py
@@ -2,7 +2,7 @@
 
 import logging
 
-from django.db.models.signals import post_delete
+from django.db.models.signals import pre_delete
 from django.dispatch import receiver
 
 from dataops.sql import delete_table
@@ -11,9 +11,7 @@ from workflow.models import Workflow
 logger = logging.getLogger("project")
 
 
-@receiver(post_delete, sender=Workflow)
+@receiver(pre_delete, sender=Workflow)
 def delete_data_frame_table(sender, instance, **kwargs):
-    del sender
-    del kwargs
     if instance.has_table():
         delete_table(instance.get_data_frame_table_name())
