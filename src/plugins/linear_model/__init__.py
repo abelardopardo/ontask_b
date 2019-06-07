87d78bd0a561cd767c8c11afb0c053701ee93aa2
Abelardo Pardo <abelardo.pardo@gmail.com>
2019-06-07 12:43:56 +0930

    Linear model for the tutorial

---
+++ src/plugins/linear_model/__init__.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+
+import pandas as pd
+
+from dataops.plugin import OnTaskModel
+
+class_name = 'LinearModel'
+
+
+class LinearModel(OnTaskModel):
+    """Plugin to execute the linear model y = 3.73 * Contribution + 25.4
+
+    The result is stored in column 'Final Exam Predict'
+    """
+
+    def __init__(self):
+        """Initialize all the fields."""
+        super().__init__()
+        self.name = 'Linear Model'
+        self.description_txt = "Obtain a prediction of the final exam score."
+        self.input_column_names = ['Contribution']
+        self.output_column_names = ['Final Exam Predict']
+
+    def run(self, data_frame, parameters=dict):
+        """
+        Parse the parameters to guarantee that they were correct, and if so,
+        returns the dataframe with the resulting linear model.
+
+        :param data_frame: Input data for the plugin
+
+        :param parameters: Dictionary with (name, value) pairs.
+
+        :return: a Pandas data_frame to merge with the existing one 
+        """
+
+        new_dataframe = pd.DataFrame(3.73 * data_frame['Contribution'] + 25.4)
+        new_dataframe.columns = self.output_column_names
+
+        return new_dataframe
