7d63a9f8d4f630e033e1168024dd5903c464a597
Abelardo Pardo <abelardo.pardo@gmail.com>
2019-06-14 09:45:03 +0930

    Additional test to check how plugins are loaded.

--- src/dataops/tests/test_plugin_manager.py
+++ src/dataops/tests/test_plugin_manager.py
@@ -3,12 +3,28 @@
 """Test plugin manager functions."""
 
 import os
+
+from dataops.plugin import OnTaskModel
+from dataops.plugin.plugin_manager import _verify_plugin
 import test
 
 from django.conf import settings
 from rest_framework import status
 
 from dataops.models import Plugin
+
+class BogusPlugin(object):
+    """Bogus class to be used for testing."""
+    pass
+
+
+class BogusPlugin2(OnTaskModel):
+
+    def __init__(self):
+        super().__init__()
+        self.input_column_names = 3
+        self.output_column_names = 3
+        self.parameters = 3
 
 
 class DataopsTransform(test.OnTaskTestCase):
@@ -36,3 +52,24 @@ class DataopsTransform(test.OnTaskTestCase):
 
         resp = self.get_response('dataops:model')
         self.assertTrue(status.is_success(resp.status_code))
+
+
+class DataopsPluginErrors(test.OnTaskTestCase):
+    """Test the error detection for plugins."""
+
+    def test_condition_1(self):
+        """Class is subclass of OnTaskPlubinAbstract"""
+        pinobj = BogusPlugin()
+        tests = _verify_plugin(pinobj)
+        self.assertTrue(tests[0][0] != 'Ok')
+
+    def test_condition_2(self):
+        """Class is not documented, no fields."""
+        pinobj = BogusPlugin2()
+        tests = _verify_plugin(pinobj)
+        self.assertTrue(tests[1][0] != 'Ok')
+        self.assertTrue(tests[2][0] != 'Ok')
+        self.assertTrue(tests[3][0] != 'Ok')
+        self.assertTrue(tests[4][0] != 'Ok')
+        self.assertTrue(tests[5][0] != 'Ok')
+        self.assertTrue(tests[6][0] != 'Ok')
