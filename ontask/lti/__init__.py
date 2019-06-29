946618424992e0e66830554cf7da2ffaf5e22404
Abelardo Pardo <abelardo.pardo@gmail.com>
2019-06-29 16:03:06 +0930

    Renamed the package to LTI

---
+++ ontask/lti/__init__.py
@@ -0,0 +1,8 @@
+# Classes
+from lti.outcome_request import OutcomeRequest
+from lti.outcome_response import OutcomeResponse
+from lti.tool_config import ToolConfig
+from lti.tool_consumer import ToolConsumer
+from lti.tool_provider import ToolProvider
+# Exceptions
+from lti.utils import InvalidLTIConfigError, InvalidLTIRequestError
