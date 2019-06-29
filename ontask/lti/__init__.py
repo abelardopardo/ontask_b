0cfbb98aa409703c4013b0c29afc961f1b66c1cb
Abelardo Pardo <abelardo.pardo@gmail.com>
2019-06-29 17:46:33 +0930

    Added the right prefix to imports.

--- ontask/lti/__init__.py
+++ ontask/lti/__init__.py
@@ -1,8 +1,8 @@
 # Classes
-from lti.outcome_request import OutcomeRequest
-from lti.outcome_response import OutcomeResponse
-from lti.tool_config import ToolConfig
-from lti.tool_consumer import ToolConsumer
-from lti.tool_provider import ToolProvider
+from ontask.lti.outcome_request import OutcomeRequest
+from ontask.lti.outcome_response import OutcomeResponse
+from ontask.lti.tool_config import ToolConfig
+from ontask.lti.tool_consumer import ToolConsumer
+from ontask.lti.tool_provider import ToolProvider
 # Exceptions
-from lti.utils import InvalidLTIConfigError, InvalidLTIRequestError
+from ontask.lti.utils import InvalidLTIConfigError, InvalidLTIRequestError
