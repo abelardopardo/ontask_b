adf7fceda31897afa7f484428bd5193f45ecbec8
Abelardo Pardo <abelardo.pardo@gmail.com>
2019-07-19 18:45:56 +0930

    New code to polish visualization.

--- ontask/visualizations/plotly.py
+++ ontask/visualizations/plotly.py
@@ -3,20 +3,18 @@
 Implementation of visualizations using the Plotly JS library
 """
 
-import json
 from builtins import str
+import json
+
+from django.utils.translation import ugettext as _
 
 from ontask.dataops.pandas import pandas_datatype_names
-
-from . import VisHandler
+from ontask.visualizations import VisHandler
 
 
 class PlotlyHandler(VisHandler):
-    """
-    Handler to produce Plotly visualizations.
-    """
+    """Handler to produce Plotly visualizations."""
 
-    # head_scripts = ["//cdn.plot.ly/plotly-latest.min.js"]
     head_scripts = [
         "//cdn.plot.ly/plotly-cartesian-latest.min.js"
     ]
@@ -31,14 +29,15 @@ class PlotlyHandler(VisHandler):
         super().__init__(data, *args, **kwargs)
 
         self.format_dict = {
-            'style': ''
+            'style': 'width: 100%; height:100%;'
         }
 
-        self.layout = {'margin': {'l': 35, 'r': 35, 't': 35, 'b': 35}}
+        self.layout = {'autosize': True}
 
     @staticmethod
     def get_engine_scripts(current=None):
-        """
+        """Return the scripts required for the HTML rendering.
+
         Return the HTML HEAD snippet including whatever <scripts> are required
         :return: String to include in HEAD
         """
@@ -51,17 +50,15 @@ class PlotlyHandler(VisHandler):
         return current
 
     def render(self):
-        """
-        Return the rendering in HTML fo this visualization
+        """Return the rendering in HTML fo this visualization.
+
         :return: String as HTML snippet
         """
         return self.html_content
 
 
 class PlotlyBoxPlot(PlotlyHandler):
-    """
-    Create a boxplot with a given data frame column
-    """
+    """Create a boxplot with a given data frame column."""
 
     def __init__(self, data, *args, **kwargs):
 
@@ -91,14 +88,16 @@ class PlotlyBoxPlot(PlotlyHandler):
                 'ay': 0,
                 'xref': 'x',
                 'yref': 'y',
-                'text': self.format_dict.get('individual_text', 'Value')
+                'text': self.format_dict.get(
+                    'individual_text',
+                    _('Your value'))
             }]
 
             # Get the two custom values from the given parameters.
             self.layout['annotations'][0]['y'] = \
                 self.format_dict['individual_value']
             self.layout['annotations'][0]['text'] = \
-                self.format_dict.get('individual_text', 'Value')
+                self.format_dict.get('individual_text', _('Your value'))
 
         # Redefine the layout
         self.format_dict['layout'] = json.dumps(self.layout)
@@ -122,21 +121,14 @@ class PlotlyBoxPlot(PlotlyHandler):
 
 
 class PlotlyColumnHistogram(PlotlyHandler):
-    """
-    Create a histogram
-    """
+    """Create a histogram."""
 
-    def __init__(self, data, *args, **kwargs):
-
-        super().__init__(data, *args, **kwargs)
-
+    def _create_dictionaries(self, data, *args, **kwargs):
         self.format_dict['id'] = 'histogram-id'
 
-        self.layout.update(
-            {'autobinx': True,
-             'autobiny': True,
-             'bargap': 0.01,
-             'yaxis': {'title': 'Count'}})
+        self.layout.update({
+            'bargap': 0.01,
+            'yaxis': {'title': 'Count'}})
 
         # Transfer the keys to the formatting dictionary
         for key, value in list(kwargs.pop('context', {}).items()):
@@ -151,18 +143,17 @@ class PlotlyColumnHistogram(PlotlyHandler):
         # Special case for bool and datetime. Turn into strings to be
         # treated as such
         if column_dtype == 'boolean' or column_dtype == 'datetime' or \
-                column_dtype == 'string':
+            column_dtype == 'string':
             data_list = [str(x) for x in data_list]
 
         data.append(
             {'x': data_list,
-             'autobinx': True,
-             'histnorm': 'count',
+             'histnorm': '',
              'name': column,
              'type': 'histogram'}
         )
 
-        self.format_dict['data'] = json.dumps(data)
+        self.format_dict['data'] = data
 
         # If an individual value has been given, add the annotation and the
         # layout to the rendering.
@@ -177,12 +168,12 @@ class PlotlyColumnHistogram(PlotlyHandler):
                 'ax': 0,
                 'axref': 'pixel',
                 'y': 0,
+                'ay': -40,
                 'yref': 'paper',
-                'yshift': 'bottom',
-                'text': self.format_dict.get('individual_text', 'Value')
+                'text': self.format_dict.get('individual_text', 'Your value')
             }]
 
-        self.format_dict['layout'] = json.dumps(self.layout)
+        self.format_dict['layout'] = self.layout
 
         self.html_content = ''
         if self.format_dict.get('title'):
@@ -190,77 +181,26 @@ class PlotlyColumnHistogram(PlotlyHandler):
 
         self.html_content += self.html_skel.format(**self.format_dict)
 
-    def get_id(self):
-        """
-        Return the name of this handler
-        :return: string with the name
-        """
-        return self.format_dict['id']
-
-
-class PlotlyGauge(PlotlyHandler):
-    """
-    Create a gauge pointing to a value
-    """
-
-    # FIX FIX FIX
-    format_dict = {
-        'id': 'histogram-id',
-        'data': "[{ y: [], type: 'histogram'}]",
-        'layout': "{}"
-    }
-
-    # FIX FIX FIX
-    layout = {
-        'shapes': [{
-            'type': 'path',
-            'path': None,
-            'fillcolor': '850000',
-            'line': {'color': '850000'}
-        }],
-        'title': 'Gauge Speed 0 - 100',
-        'height': 400,
-        'width': 400,
-        'xaxis': {
-            'zeroline': False,
-            'showticklabels': False,
-            'showgrid': False,
-            'range': [-1, 1]},
-        'yaxis': {
-            'zeroline': False,
-            'showticklabels': False,
-            'showgrid': False,
-            'range': [-1, 1]}
-    }
-
     def __init__(self, data, *args, **kwargs):
-        # Transfer the keys to the formatting dictionary
-        for key, value in list(kwargs.pop('context', {}).items()):
-            self.format_dict[key] = value
-
+        """Create the new object."""
         super().__init__(data, *args, **kwargs)
 
-        data = []
-        for column in self.data.columns:
-            data.append(
-                {'x': list(self.data[column].dropna()),
-                 'autobinx': True,
-                 'histnorm': 'count',
-                 'name': column,
-                 'type': 'histogram'}
-            )
-        self.format_dict['data'] = json.dumps(data)
+        # Create the dictionaries with values
+        self._create_dictionaries(data, *args, **kwargs)
 
-        self.layout['bargap'] = 0.01
-        self.layout['yaxis'] = {'title': 'Count'}
+        self.html_content = ''
+        if self.format_dict.get('title'):
+            self.html_content = self.format_dict['title']
 
-        self.format_dict['layout'] = json.dumps(self.layout)
-
-        self.html_content = self.html_skel.format(**self.format_dict)
+        self.html_content += self.html_skel.format(
+            style=self.format_dict['style'],
+            id=self.format_dict['id'],
+            data=json.dumps(self.format_dict['data']),
+            layout=json.dumps(self.format_dict['layout']))
 
     def get_id(self):
-        """
-        Return the name of this handler
+        """Return the name of this handler.
+
         :return: string with the name
         """
         return self.format_dict['id']
