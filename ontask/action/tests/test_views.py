ad40a13d28d799dbbef074f886653d8ddc813ac1
Abelardo Pardo <abelardo.pardo@gmail.com>
2019-08-31 19:44:07 +0930

    Additional changes due to new boolean manipulation

--- ontask/action/tests/test_views.py
+++ ontask/action/tests/test_views.py
@@ -598,7 +598,7 @@ class ActionActionInCreate(test.OnTaskLiveTestCase):
         self.create_new_survey_action('new action in', '')
 
         # Click in the add rule button (the filter is initially empty)
-        self.create_filter('', [('registered', 'equals', 'No')])
+        self.create_filter('', [('registered', 'equal', 'false')])
         self.wait_close_modal_refresh_table('column-selected-table_previous')
 
         # Check that the filter is working properly
