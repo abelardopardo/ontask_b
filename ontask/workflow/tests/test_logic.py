64a326151284ea1e6c22c33765a260d4b33479ea
Abelardo Pardo <abelardo.pardo@gmail.com>
2019-09-07 19:50:03 +0930

    Cosmetic changes about test files

--- ontask/workflow/tests/test_logic.py
+++ ontask/workflow/tests/test_logic.py
@@ -11,11 +11,7 @@ from django.conf import settings
 from django.contrib.auth import get_user_model
 from django.utils.six import BytesIO
 from rest_framework.parsers import JSONParser
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.wait import WebDriverWait
 
-from ontask.dataops.pandas import destroy_db_engine
 from ontask.models import Workflow
 from ontask.workflow.import_export import (
     do_export_workflow, do_export_workflow_parse, do_import_workflow_parse,
@@ -61,105 +57,6 @@ class WorkflowImportExport(test.OnTaskTestCase):
                          data['ncols'])
         self.assertEqual(workflow.attributes, data['attributes'])
         self.assertEqual(workflow.query_builder_ops, data['query_builder_ops'])
-
-
-class WorkflowImport(test.OnTaskLiveTestCase):
-    fixtures = ['simple_workflow_export']
-    filename = os.path.join(
-        settings.BASE_DIR(),
-        'ontask',
-        'fixtures',
-        'simple_workflow_export.sql'
-    )
-
-    def setUp(self):
-        super().setUp()
-        test.pg_restore_table(self.filename)
-
-    def tearDown(self):
-        test.delete_all_tables()
-        super().tearDown()
-
-    def test_import_complete(self):
-
-        # Login and wait for the table of workflows
-        self.login('instructor01@bogus.com')
-
-        # Click in the import button and wait
-        self.selenium.find_element_by_link_text('Import workflow').click()
-        WebDriverWait(self.selenium, 10).until(
-            EC.text_to_be_present_in_element((By.XPATH, "//body/div/h1"),
-                                             'Import workflow')
-        )
-
-        # Set the workflow name and file
-        wname = self.selenium.find_element_by_id('id_name')
-        wname.send_keys('newwf')
-        wfile = self.selenium.find_element_by_id('id_wf_file')
-        wfile.send_keys(os.path.join(settings.BASE_DIR(),
-                                     'ontask',
-                                     'fixtures',
-                                     'ontask_workflow.gz'))
-
-        # Click in the submit
-        self.selenium.find_element_by_xpath(
-            "//button[@type='Submit']"
-        ).click()
-        WebDriverWait(self.selenium, 20).until(
-            EC.presence_of_element_located(
-                (By.XPATH,
-                 "//h5[contains(@class, 'card-header') "
-                 "and normalize-space(text()) = '{0}']".format('newwf')),
-            )
-        )
-
-        # Check elements in workflow and in newwf
-        w1 = Workflow.objects.get(name=test.wflow_name)
-        w2 = Workflow.objects.get(name='newwf')
-
-        # Equal descriptions
-        self.assertEqual(w1.description_text,
-                         w2.description_text)
-
-        # Equal number of columns
-        self.assertEqual(w1.columns.count(), w2.columns.count())
-
-        # Identical attributes
-        self.assertEqual(w1.attributes, w2.attributes)
-
-        # Equal number of rows and columns
-        self.assertEqual(w1.nrows, w2.nrows)
-        self.assertEqual(w1.ncols, w2.ncols)
-
-        # Equal names and column types
-        for x, y in zip(w1.columns.all(), w2.columns.all()):
-            self.assertEqual(x.name, y.name)
-            self.assertEqual(x.data_type, y.data_type)
-            self.assertEqual(x.is_key, y.is_key)
-
-        # Equal number of actions
-        self.assertEqual(w1.actions.count(),
-                         w2.actions.count())
-
-        # Equal names and content in the conditions
-        for x, y in zip(w1.actions.all(), w2.actions.all()):
-            self.assertEqual(x.name, y.name)
-            self.assertEqual(x.description_text, y.description_text)
-            self.assertEqual(x.text_content, y.text_content)
-            self.assertEqual(x.conditions.count(),
-                             y.conditions.count())
-            for c1, c2 in zip(x.conditions.all(), y.conditions.all()):
-                self.assertEqual(c1.name, c2.name)
-                self.assertEqual(c1.description_text,
-                                 c2.description_text)
-                self.assertEqual(c1.formula, c2.formula)
-                self.assertEqual(c1.is_filter, c2.is_filter)
-
-        # End of session
-        self.logout()
-
-        # Close the db_engine
-        destroy_db_engine()
 
 
 class WorkflowImportExportCycle(test.OnTaskTestCase):
