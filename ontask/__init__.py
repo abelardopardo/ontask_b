dc6de25c292fc8bd764c8f8de349fb3401dcb226
Abelardo Pardo <abelardo.pardo@gmail.com>
2019-10-03 21:26:36 +0930

    New test for issues #128 and #168

--- ontask/__init__.py
+++ ontask/__init__.py
@@ -93,14 +93,6 @@ def is_correct_email(email_txt):
     return True
 
 
-def is_json(text):
-    try:
-        _ = json.loads(text)
-    except ValueError:
-        return False
-    return True
-
-
 def simplify_datetime_str(dtime):
     return dtime.astimezone(
         pytz.timezone(django.conf.settings.TIME_ZONE)
