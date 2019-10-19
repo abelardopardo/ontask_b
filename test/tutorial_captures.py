be80300d1fab170973484c301b44636ce2f4eaa8
Abelardo Pardo <abelardo.pardo@gmail.com>
2019-10-19 16:56:18 +1030

    Changed test form detection due to library upgrade

--- test/tutorial_captures.py
+++ test/tutorial_captures.py
@@ -188,7 +188,7 @@ class TutorialCaptures(ScreenTests):
         self.selenium.find_element_by_link_text('Import actions').click()
         WebDriverWait(self.selenium, 10).until(
             EC.element_to_be_clickable(
-                (By.XPATH, '//input[@id="id_upload_file"]')
+                (By.XPATH, '//button[@name="Submit"]')
             )
         )
         self.body_ss('tutorial_action_import.png')
