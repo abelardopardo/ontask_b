31e97f1e31c4e5d155d659a7419b294740626b36
Abelardo Pardo <abelardo.pardo@gmail.com>
2019-11-09 07:51:15 +1030

    Remove leftover code

--- ontask/action/send/__init__.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-
-"""Module containing the functions to send messages, JSON."""
-
-from ontask.action.services.canvas_email import send_canvas_emails
-from ontask.action.services.json import send_json, send_json_list
