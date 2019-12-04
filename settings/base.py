2119e6e4b3a876934ee29c0014977f0fe145f89c
Abelardo Pardo <abelardo.pardo@gmail.com>
2019-12-04 16:53:37 +1100

    Refactored the configuration of loggers

--- settings/base.py
+++ settings/base.py
@@ -405,6 +405,60 @@ LANGUAGES = (
 
 ###############################################################################
 #
+# LOGGER (modified further in development/production)
+#
+###############################################################################
+LOGGING_CONFIG = None
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': (
+                '[%(asctime)s] %(levelname)s '
+                + '[%(pathname)s:%(lineno)s] %(message)s'),
+            'datefmt': "%d/%b/%Y %H:%M:%S"
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        },
+    },
+    'handlers': {
+        'django_log_file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': join(LOG_FOLDER, 'django.log'),
+            'formatter': 'verbose'
+        },
+        'ontask_log_file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': join(LOG_FOLDER, 'ontask.log'),
+            'formatter': 'verbose'
+        },
+        'script_log_file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': join(LOG_FOLDER, 'script.log'),
+            'formatter': 'verbose'
+        },
+        'celery_log_file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': join(LOG_FOLDER, 'celery.log'),
+            'formatter': 'verbose'
+        },
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'verbose'
+        }
+    },
+}
+
+
+###############################################################################
+#
 # SUMMERNOTE CONFIGURATION
 #
 ###############################################################################
