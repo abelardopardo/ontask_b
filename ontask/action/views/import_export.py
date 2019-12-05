8fe08551fcaa9cfe3f257417d11ca2fc7dd57031
Abelardo Pardo <abelardo.pardo@gmail.com>
2019-12-05 09:02:32 +0900

    Missing file.

---
+++ ontask/action/views/import_export.py
@@ -0,0 +1,122 @@
+# -*- coding: utf-8 -*-
+
+"""Views for import/export."""
+import datetime
+import gzip
+from io import BytesIO
+from typing import Optional
+
+from django.contrib import messages
+from django.contrib.auth.decorators import user_passes_test
+from django.db import transaction
+from django.http.request import HttpRequest
+from django.http.response import HttpResponse
+from django.shortcuts import redirect, render
+from django.utils.translation import ugettext_lazy as _
+from rest_framework.renderers import JSONRenderer
+
+from ontask import models
+from ontask.action import forms, serializers
+from ontask.action.services import do_import_action
+from ontask.core import get_workflow, is_instructor
+
+
+@user_passes_test(is_instructor)
+@get_workflow(pf_related='actions')
+def action_import(
+    request: HttpRequest,
+    workflow: Optional[models.Workflow] = None,
+) -> HttpResponse:
+    """Import one action given in a gz file.
+
+    :param request: Http request
+    :param workflow: Workflow being manipulated (set by the decorators)
+    :return: HTTP response
+    """
+    form = forms.ActionImportForm(request.POST or None, request.FILES or None)
+
+    if request.method == 'POST' and form.is_valid():
+        # Process the reception of the file
+        if not form.is_multipart():
+            form.add_error(
+                None,
+                _('Incorrect form (it does not have a file attached)'))
+            return render(request, 'action/import.html', {'form': form})
+
+        # UPLOAD THE FILE!
+        try:
+            with transaction.atomic():
+                actions = do_import_action(
+                    request.user,
+                    workflow,
+                    request.FILES['upload_file'])
+        except KeyError as exc:
+            # Attach the exception to the request
+            messages.error(
+                request,
+                _('Unable to import file. Incorrect fields.').format(str(exc)),
+            )
+            return redirect('action:index')
+        except Exception as exc:
+            # Attach the exception to the request
+            messages.error(
+                request,
+                _('Unable to import file: {0}').format(str(exc)),
+            )
+            return redirect('action:index')
+
+        messages.success(
+            request,
+            _('Actions imported: {0}'.format(', '.join(
+                [action.name for action in actions]
+            )))
+        )
+        # Go back to the list of actions
+        return redirect('action:index')
+
+    return render(request, 'action/import.html', {'form': form})
+
+
+@user_passes_test(is_instructor)
+@get_workflow(pf_related='actions')
+def export(
+    request: HttpRequest,
+    pklist: str,
+    workflow: Optional[models.Workflow] = None,
+) -> HttpResponse:
+    """Export the actions given as comma separated list of ids.
+
+    :param request:
+    :param pklist: comma separated list of action ids as strs
+    :param workflow: Set by the decorators (current workflow)
+    :return: HTTP response
+    """
+    del request
+    try:
+        action_ids = [int(a_idx) for a_idx in pklist.split(',')]
+    except ValueError:
+        return redirect('home')
+
+    # Serialize the content and return data
+    serializer = serializers.ActionSelfcontainedSerializer(
+        instance=workflow.actions.filter(id__in=action_ids),
+        many=True,
+        required=True)
+
+    # Get the in-memory file to compress
+    zbuf = BytesIO()
+    zfile = gzip.GzipFile(mode='wb', compresslevel=6, fileobj=zbuf)
+    zfile.write(JSONRenderer().render(serializer.data))
+    zfile.close()
+
+    # Attach the compressed value to the response and send
+    compressed_content = zbuf.getvalue()
+    response = HttpResponse(compressed_content)
+    response['Content-Type'] = 'application/octet-stream'
+    response['Content-Transfer-Encoding'] = 'binary'
+    response['Content-Disposition'] = (
+        'attachment; filename="ontask_actions_{0}.gz"'.format(
+            datetime.datetime.now().strftime('%y%m%d_%H%M%S')))
+    response['Content-Length'] = str(len(compressed_content))
+
+    return response
