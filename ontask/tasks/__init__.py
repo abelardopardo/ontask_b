7fa8688ac8473c29652c1e5cf1dc7ac94253af6f
Abelardo Pardo <abelardo.pardo@gmail.com>
2019-12-12 07:42:15 +1030

    Removed unnecessary imports

--- ontask/tasks/__init__.py
+++ ontask/tasks/__init__.py
@@ -1,7 +1,4 @@
 # -*- coding: utf-8 -*-
 
 from ontask.tasks.execute import execute_operation, task_execute_factory
-from ontask.tasks.increase_track import ExecuteIncreaseTrackCount
-from ontask.tasks.run_plugin import ExecuteRunPlugin
 from ontask.tasks.scheduled_ops import execute_scheduled_operation
-from ontask.tasks.workflow_update_luser import ExecuteUpdateWorkflowLUser
