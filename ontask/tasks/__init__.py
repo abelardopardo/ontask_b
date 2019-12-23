908b0a95525994ade4241a9c234e88d6aed88067
Abelardo Pardo <abelardo.pardo@gmail.com>
2019-12-24 07:53:36 +1030

    Moved factory initialisation to the init file

--- ontask/tasks/__init__.py
+++ ontask/tasks/__init__.py
@@ -1,4 +1,22 @@
 # -*- coding: utf-8 -*-
 
+"""Import packages and initialize the task_execute_factory."""
+from ontask import models
 from ontask.tasks.execute import execute_operation, task_execute_factory
+from ontask.tasks.increase_track import ExecuteIncreaseTrackCount
+from ontask.tasks.run_plugin import ExecuteRunPlugin
 from ontask.tasks.scheduled_ops import execute_scheduled_operation
+from ontask.tasks.workflow_update_luser import ExecuteUpdateWorkflowLUser
+
+
+task_execute_factory.register_producer(
+    models.Log.WORKFLOW_UPDATE_LUSERS,
+    ExecuteUpdateWorkflowLUser())
+
+task_execute_factory.register_producer(
+    models.Log.WORKFLOW_INCREASE_TRACK_COUNT,
+    ExecuteIncreaseTrackCount())
+
+task_execute_factory.register_producer(
+    models.Log.PLUGIN_EXECUTE,
+    ExecuteRunPlugin())
