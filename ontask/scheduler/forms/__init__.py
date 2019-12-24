1fcf93671307b72dc18844eb5420dd2ebb506939
Abelardo Pardo <abelardo.pardo@gmail.com>
2019-12-24 17:54:30 +1030

    Forms are now broken into various files

--- ontask/scheduler/forms/__init__.py
+++ ontask/scheduler/forms/__init__.py
@@ -2,3 +2,7 @@
 
 """Import the forms"""
 from ontask.scheduler.forms.basic import ScheduleBasicForm
+from ontask.scheduler.forms.run import (
+    ScheduleEmailForm, ScheduleJSONForm, ScheduleJSONListForm,
+    ScheduleSendListForm)
+from ontask.scheduler.forms.sql import ScheduleSQLUploadForm
