29c75538740d722b67e1bbc13be4ad73ae94cefa
Abelardo Pardo <abelardo.pardo@gmail.com>
2020-01-19 14:40:56 +1030

    Upgraded the plugins to match required structure

--- lib/plugins/test_plugin_1/__init__.py
+++ lib/plugins/test_plugin_1/__init__.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 
+from typing import Dict, Optional
+
 import pandas as pd
 
 from ontask.dataops.plugin import OnTaskTransformation
@@ -111,7 +113,7 @@ class OnTaskTestPlugin(OnTaskTransformation):
              'help param datetime'),
         ]
 
-    def run(self, data_frame, parameters=dict):
+    def run(self, data_frame: pd.DataFrame, parameters: Optional[Dict] = dict):
         """
         Method to overwrite. Receives a data frame wih a number of columns
         stipulated by the num_column_input pair and a dictionary with parameters
