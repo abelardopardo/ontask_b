cc4cea510ab3b23df982a93d99af58990f1d0776
Abelardo Pardo <abelardo.pardo@gmail.com>
2023-03-30 08:51:36 +1030

    File with functionality for athena connection

---
+++ ontask/admin/athenaconnection.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+
+"""Classes for Athena connection admin."""
+from django.contrib import admin
+
+from ontask import models
+
+
+@admin.register(models.AthenaConnection)
+class AthenaConnectionAdmin(admin.ModelAdmin):
+    """Admin for the Athena Connections."""
+
+    list_display = (
+        'id',
+        'name',
+        'description_text',
+        'aws_access_key',
+        'aws_secret_access_key',
+        'aws_session_token',
+        'aws_bucket_name',
+        'aws_file_path',
+        'aws_region_name',
+        'table_name')
+
+    search_fields = [
+        'name',
+        'description_text',
+        'aws_access_key',
+        'aws_secret_access_key',
+        'aws_session_token',
+        'aws_bucket_name',
+        'aws_file_path',
+        'aws_region_name',
+        'table_name']
