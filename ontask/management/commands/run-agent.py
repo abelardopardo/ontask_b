59c2e7f5a7ae785c8bf5650634535179bdfadb75
Leon Romo <leonkim@Leons-MacBook-Air.local>
2023-11-24 16:36:11 +1030

    Update ontask running successfully

--- ontask/management/commands/run-agent.py
+++ ontask/management/commands/run-agent.py
@@ -12,9 +12,6 @@ if os.path.exists(AGENT_ENV_FILENAME):
 
 from ontask.agent.utils.authenticate import authenticate_canvas
 from ontask.agent.data_operations.data_ops import (
-    fetch_canvas_data_submissions,
-    fetch_ontask_data,
-    data_has_changes,
     update_ontask_table,
     fetch_canvas_data_quiz_stats
 )
@@ -32,18 +29,25 @@ class Command(BaseCommand):
             update_success = update_ontask_table(resource, wid)
             if update_success:
                 logging.info(f"The OnTask table was successfully updated with data from Canvas for WID {wid}.")
+                self.stdout.write(self.style.SUCCESS("The OnTask table was successfully updated with data from Canvas for WID {}.".format(wid)))
+
             else:
                 logging.error(f"Failed to update the OnTask table with data from Canvas for WID {wid}.")
+                self.stdout.write(self.style.ERROR("Failed to update the OnTask table with data from Canvas"))
         else:
             logging.error(f"No data was fetched from Canvas, or the data is not in the expected format for WID {wid}.")
+            self.stdout.write(self.style.ERROR("No data was fetched from Canvas, or the data is not in the expected format"))
 
     def handle(self, *args, **kwargs):
         if not authenticate_canvas():
             logging.error('Failed to authenticate with Canvas.')
+            self.stdout.write(self.style.ERROR("Failed to authenticate with Canvas"))
             return
+        else:
+            self.stdout.write(self.style.SUCCESS("Authenticated with Canvas successfully!"))
 
         # Load the course config from env
-        courses_config = json.loads(self.env('PYTHON_COURSE', default="{}"))
+        courses_config = json.loads(self.env('LIST_OF_WORKFLOW_ID_AND_COURSE_ID', default="{}"))
 
         for wid, course_info in courses_config.items():
             course_id = course_info.get('course_id')
@@ -56,3 +60,4 @@ class Command(BaseCommand):
                     self.__process_resource__(wid, resource)
             else:
                 logging.error(f"Workflow ID {wid} is missing course_id or quizzes in the configuration.")
+            self.stdout.write(self.style.SUCCESS('Agent operation completed.'))
